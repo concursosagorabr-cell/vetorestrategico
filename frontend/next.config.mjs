e05476c5c49dd4f4810611604378f9faa1e1bd72
Marco Agostino <marco.agostinoo@hotmail.com>
2026-08-20 14:12:41 -0300

    fix: habilitar unoptimized true para carregamento imediato de todas as imagens na Vercel

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -2,7 +2,7 @@
 const nextConfig = {
   reactStrictMode: true,
   images: {
-    formats: ['image/avif', 'image/webp'],
+    unoptimized: true,
     remotePatterns: [
       {
         protocol: 'https',
